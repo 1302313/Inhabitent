503cd5ae350f44db4549e6cc9421d924e2b57fbc
AdhamElNewihy <adham.el.newihy@gmail.com>
2019-08-29 12:58:28 -0700

    Update Journal Archive design

--- themes/inhabitent/home.php
+++ themes/inhabitent/home.php
@@ -8,47 +8,50 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
+<div id="primary" class="content-area page-journal">
     <main id="main" class="site-main" role="main">
 
         <?php if (have_posts()) : ?>
 
-            <?php if (is_home() && !is_front_page()) : ?>
-                <header>
-                    <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-                </header>
-            <?php endif; ?>
+        <?php if (is_home() && !is_front_page()) : ?>
+        <header>
+            <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+        </header>
+        <?php endif; ?>
 
-            <?php /* Start the Loop */ ?>
-            <?php while (have_posts()) : the_post(); ?>
+        <?php /* Start the Loop */ ?>
+        <?php while (have_posts()) : the_post(); ?>
 
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <header class="entry-header">
-                        <?php if (has_post_thumbnail()) : ?>
-                            <?php the_post_thumbnail('large'); ?>
-                        <?php endif; ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header journal-image-container">
 
-                        <?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
+                <?php if (has_post_thumbnail()) : ?>
+                <?php the_post_thumbnail('large'); ?>
+                <?php endif; ?>
 
-                        <?php if ('post' === get_post_type()) : ?>
-                            <div class="entry-meta">
-                                <?php red_starter_posted_on(); ?> / <?php comments_number('0 Comments', '1 Comment', '% Comments'); ?> / <?php red_starter_posted_by(); ?>
-                            </div><!-- .entry-meta -->
-                        <?php endif; ?>
-                    </header><!-- .entry-header -->
+                <?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
 
-                    <div class="entry-content">
-                        <?php the_excerpt(); ?>
-                    </div><!-- .entry-content -->
-                </article><!-- #post-## -->
+                <?php if ('post' === get_post_type()) : ?>
+                <div class="entry-meta">
+                    <?php red_starter_posted_on(); ?> / <?php comments_number('0 Comments', '1 Comment', '% Comments'); ?> / <?php red_starter_posted_by(); ?>
+                </div><!-- .entry-meta -->
+                <?php endif; ?>
+            </header><!-- .entry-header -->
 
-            <?php endwhile; ?>
+            <div class="entry-content">
+                <?php the_excerpt(); ?>
+            </div><!-- .entry-content -->
 
-            <?php the_posts_navigation(); ?>
+            <a class="white-btn">Read More →</a>
+        </article><!-- #post-## -->
+
+        <?php endwhile; ?>
+
+        <?php the_posts_navigation(); ?>
 
         <?php else : ?>
 
-            <?php get_template_part('template-parts/content', 'none'); ?>
+        <?php get_template_part('template-parts/content', 'none'); ?>
 
         <?php endif; ?>
 
