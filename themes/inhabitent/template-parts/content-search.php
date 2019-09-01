2e428f36aab293adfab1d4de5d217be6d08e922c
AdhamElNewihy <adham.el.newihy@gmail.com>
2019-09-01 13:49:23 -0700

    Update spacing design of widgets, content-search and content-none

--- themes/inhabitent/template-parts/content-search.php
+++ themes/inhabitent/template-parts/content-search.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying results in search pages.
  *
@@ -9,11 +10,11 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
+		<?php if ('post' === get_post_type()) : ?>
 		<div class="entry-meta">
-			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
+			<?php red_starter_posted_on(); ?> / <?php comments_number('0 Comments', '1 Comment', '% Comments'); ?> / <?php red_starter_posted_by(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
@@ -21,4 +22,8 @@
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
+
+	<p><a href="<?php echo get_post_permalink()  ?>" class="white-btn">Read more →</a></p>
+
+
 </article><!-- #post-## -->
